92839373285ed2c22ff2f2efcbff6e171541d664
Joe Roberts <jroberts@palisade.co>
2025-08-01 08:20:01 +0100

    build: migrate to IntelliJ Platform 2025.1.3 and Gradle Plugin 2.6.0
    - Update IntelliJ Platform to 2025.1.3
    - Migrate from IntelliJ Platform Gradle Plugin 1.x to 2.6.0
    - Update platform compatibility range to 232-251.*
    - Fix deprecated Gradle API usage
    - Add test framework configuration
    - Configure plugin signing and publishing with new API
    - Update Earthfile to match new build configuration
    
    This migration ensures compatibility with the latest IntelliJ IDEA versions
    while maintaining backwards compatibility to 2023.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,65 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
+}
+
+group = "dev.earthly"
+version = "1.0.2"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     implementation("org.apache.commons:commons-collections4:4.4")
     testImplementation("junit:junit:4.13.2")
+    
+    intellijPlatform {
+        intellijIdeaUltimate("2025.1.3")
+        bundledPlugin("org.jetbrains.plugins.textmate")
+        
+        pluginVerifier()
+        zipSigner()
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    }
 }
 
-group = "dev.earthly"
-version = "0.0.0"
-
-repositories {
-    mavenCentral()
+intellijPlatform {
+    buildSearchableOptions = false
+    
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "232"
+            untilBuild = "251.*"
+        }
+    }
+    
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+    
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
+    
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
-
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    version.set("2023.2.6")
-    type.set("IU") // Target IDE Platform
-    plugins.set(listOf("org.jetbrains.plugins.textmate"))
-}
-
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
-
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("243.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-
+    
     test {
         useJUnit()
     }
